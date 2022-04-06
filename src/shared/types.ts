51d0b76c3dcc0cb6ab7ff780ea30000aa6332c58
Luka <lukakobaidze2006@gmail.com>
2022-04-06 16:29:39 +0400

    types file

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,7 +1,4 @@
-export type Gamemode = 'RockPaperScissors' | 'RockPaperScissorsLizardSpock';
-
 export const rockPaperScissorsButtons = ['rock', 'paper', 'scissors'] as const;
-
 export const rockPaperScissorsLizardSpockButtons = [
   'rock',
   'paper',
@@ -10,6 +7,6 @@ export const rockPaperScissorsLizardSpockButtons = [
   'spock',
 ] as const;
 
+export type Gamemode = 'RockPaperScissors' | 'RockPaperScissorsLizardSpock';
 export type GameButtons = 'rock' | 'paper' | 'scissors' | 'lizard' | 'spock';
-
 export type Result = 'win' | 'lose' | 'tie';
